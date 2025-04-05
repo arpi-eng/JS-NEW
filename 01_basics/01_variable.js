bea429863fa6930d0f4ed941f0db98383e95a5f0
Abhigyan Dwivedi <84132249+arpi-eng@users.noreply.github.com>
2025-04-05 09:06:57 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = "12345";
+let accountEmail = "abhigyan@gmail.com";
+let accountPassword = "1234567890"; 
+accountCity = "bhopal";   //do not prefer this way of declaring variables
+
+accountEmail = "231@gmail.com";
+accountPassword = "ewpass";
+accountCity = "indore"; 
+
+/*
+do not use var in modern javascript
+use let and const instead
+const is used for constant variables
+let is used for variables that can be changed
+*/
+
+// Pass an object to console.table()
+console.table({accountEmail, accountPassword, accountCity});
+
+
