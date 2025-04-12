26fe0a309b3900c59ebb8f6ef1f2fb84f37143c4
Abhigyan Dwivedi <84132249+arpi-eng@users.noreply.github.com>
2025-04-13 01:57:08 +0530

    Update 03_convandoperations.js

--- 01_basics/03_convandoperations.js
+++ 01_basics/03_convandoperations.js
@@ -1,6 +1,27 @@
-let score = 33
+let score = undefined
 
-console.log(typeof score);
+console.log(typeof score)
 
-letvalueinnumber = Number(score)
-console.log(typeof valueInNumber);  
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// NaN is a special value in JavaScript that represents "Not a Number"
+// 33 is a number 
+// 33abc will give NaN
+// true will give 1
+// false will give 0
+// undefined will give NaN
+
+let isLoggedIn = ""
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+// 1 = true
+// 0 = false
+// "" = false
+// "abc" = true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
