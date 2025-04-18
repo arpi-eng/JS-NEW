e86b62446da16c41ecbc1393d428eb996c98e923
Abhigyan <dvandna70@gmail.com>
2025-04-18 18:46:53 +0530

    push

---
+++ 04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+
+// === strict check
+
+console.log("2" === 2);
+
+// do not do these type of conversions 
+
+console.log(null == 2);
+console.log(undefined == 2);
+console.log(null == undefined);
